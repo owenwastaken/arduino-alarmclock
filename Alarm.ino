ac9826353c6c674133dc46a8f1598b92b68609ea
Owen <71835794+owenwastaken@users.noreply.github.com>
2021-03-25 10:51:52 -0400

    Initial commit (Very Unpolished)
    This is the first commit of the Arduino alarm project.
    The code is a bit messy and it can currently only display current time onto a 16x2 character LCD.

---
+++ Alarm.ino
@@ -0,0 +1,38 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+int scn, mte, hr;
+
+void setup() {
+  lcd.begin(16, 2);
+  // start init system
+  scn = -1;
+  mte = 59;
+  hr = 23;
+}
+
+void loop() {
+  scn = scn + 1;
+  if(scn == 60)
+  {
+    scn = 0;
+    mte = mte + 1;
+    if(mte == 60)
+    {
+      mte = 0;
+      hr = hr + 1;
+      if(hr == 24)
+      {
+        hr = 0;
+      }
+    }
+  }
+  lcd.setCursor(0, 0);
+  lcd.print(hr);
+  lcd.print(":");
+  lcd.print(mte);
+  lcd.print(":");
+  lcd.print(scn);
+  lcd.print("     ");
+  delay(1000);
+}
