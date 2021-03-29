a582074a0f58f2753b312680b3e497d1f22352e5
Owen <71835794+owenwastaken@users.noreply.github.com>
2021-03-29 11:00:15 -0400

    Button Update (Still Buggy)
    -Fixed the LCD display error
    -Created basic setup script with buttons 
    
    Known Bugs:
    -Setup Script does not effect clock time
    -There is lots of delay between button press and display
    -Phantom digits appear during setup if you go from double digit to single digit
    -You can set variables higher than their max (60, and 24)

--- Alarm.ino
+++ Alarm.ino
@@ -1,21 +1,27 @@
 #include <LiquidCrystal.h>
 
-int lcd1 = 2;
-int lcd2 = 3;
-int lcd3 = 5;
-int lcd4 = 4;
-int lcd5 = 6;
-int lcd6 = 7;
+const int lcd1 = 2;
+const int lcd2 = 3;
+const int lcd3 = 4;
+const int lcd4 = 5;
+const int lcd5 = 6;
+const int lcd6 = 7;
+const int button1 = 8;
+const int button2 = 9;
+const int button3 = 10;
+
 
 LiquidCrystal lcd(lcd1, lcd2, lcd3, lcd4, lcd5, lcd6);
 int scn, mte, hr;
 
 void setup() {
+  pinMode(button1, INPUT);
+  pinMode(button2, INPUT);
+  pinMode(button3, INPUT);
   lcd.begin(16, 2);
-  // start init system
-  scn = -1;
-  mte = 0;
-  hr = 0;
+  settime(hr, "Hour", button1, button2, button3);
+  settime(mte, "Minure", button1, button2, button3);
+  settime(scn, "Second", button1, button2, button3);
 }
 
 void loop() {
@@ -42,4 +48,35 @@ void loop() {
   lcd.print(scn);
   lcd.print("     ");
   delay(1000);
+}
+
+
+void settime(int var, String varname, int uppin, int downpin, int setpin)
+{
+  int setvar = 1;
+  while(setvar == 1)
+    {
+      int upstate, downstate, setstate;
+      lcd.setCursor(0, 0);
+      lcd.print("Set ");
+      lcd.print(varname);
+      lcd.setCursor(0, 1);
+      lcd.print(var);
+      upstate = digitalRead(uppin);
+      downstate = digitalRead(downpin);
+      setstate = digitalRead(setpin);
+      if(upstate == LOW)
+      {
+        var = var + 1;
+      }
+      if(downstate == LOW)
+      {
+        var = var - 1;
+      }
+      if(setstate == LOW)
+      {
+        setvar = 0;
+      }
+      delay(500);
+    }
 }
